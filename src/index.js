ae3b0e13658589c7ca7320b48baa2ced2a1b9276
alyr.wayne <alegab89@hotmail.co.uk>
2018-01-17 22:35:21 -0700

    adding collections Rocky and Balboa

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log(`hello world 123`)
+const monk = require('monk')
+
+const url = 'mongodb://Aly:123456@ds153577.mlab.com:53577/animals'
+
+const db = monk(url)
+
+const collection = db.get('Pets')
+
+
+
+const run = async () => {
+    const myPets = [
+        { type: "turtle", breed: "fresh water", name: "Rocky", age: 15 },
+        { type: "turtle", breed: "fresh water", name: "Balboa", age: 15 }
+    ]
+    await collection.insert(myPets)
+
+    const results = await collection.find()
+
+    console.log(results)
+
+    run()
+
+}
+
+
+// collection.insert([{ type: "turtle", breed: "fresh water", name: "Rocky", age: 15 },
+// { type: "turtle", breed: "fresh water", name: "Balboa", age: 15 }])
+//     .then((docs) => {
+//     }).catch((err) => {
+//     })
+//     .then(() => {
+//         return collection.find()
+//     })
+//     .then((docs) => {
+//     })
+//     .then(() => db.close())
+
+// collection.find()
+
+
